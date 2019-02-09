eaa01169ed091f0e5040e9553416064f8fc4a917
Angelovsky <dmitrijs.angelovskis@gmail.com>
2019-02-09 17:57:07 +0200

    Add comments

--- src/test/java/JavaBasics.java
+++ src/test/java/JavaBasics.java
@@ -8,10 +8,15 @@ public class JavaBasics {
     public void javaBasic () {
         int a = 13;
         int b = 10;
+        //задать переменные (var)
+
 
         int c = sumDigits(13, 10);
+        //вызов функции и запись в var c. передача любых значений
         int d = sumDigits(a, b);
+        //вызов функции и передача уже заданных var
         int e = a + b;
+        //сложение без функции
 
         System.out.println("Sum is: " + c);
         System.out.println("Sum is: " + d);
@@ -19,12 +24,11 @@ public class JavaBasics {
 
         Assertions.assertEquals(23, c, "Sum is not correct");
     }
+    //проверяет условие. если оно не выполняется, то заваливает программу
 
 
 
-    private int sumDigits (int a, int b) {
-        return a + b;
-
+    private int sumDigits (int x, int y) {
+        return x + y;
     }
-
 }
